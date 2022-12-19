7813526282a63bd9b5418d858c9f722c06e68bfb
Georgiy Vinokurov <g.vinokurov@g.nsu.ru>
2022-12-19 14:20:58 +0700

    some errors was fixed

--- lab4/csv_parser.hpp
+++ lab4/csv_parser.hpp
@@ -2,46 +2,68 @@
 
 #include <iterator>
 #include <fstream>
+#include <limits>
 #include <string>
 #include <tuple>
+
+void skip_lines(std::ifstream & file, size_t n) {
+    file.clear(); file.seekg(0, std::ios_base::beg);
+    for (size_t i = 0; i != n; ++i)
+        file.ignore(std::numeric_limits<std::streamsize>::max(), file.widen('\n'));
+}
+
+void skip(std::ifstream & file, std::streamoff pos) {
+    file.clear(); file.seekg(pos);
+}
 
 template <typename ValueT>
 class CSVIterator final : public std::iterator<std::input_iterator_tag, ValueT, ssize_t> {
 public:
-    CSVIterator(std::ifstream & file, size_t pos) : file_(file), data_(nullptr), pos_(pos) {}
-    CSVIterator(CSVIterator const & other) : file_(other.file_), data_(new ValueT(*other.data_)), pos_(other.pos_) {}
+    static ssize_t const EOF_POS = -1;
+
+    CSVIterator(std::ifstream & file, ssize_t pos) : file_(file), item_pos_(pos), item_(nullptr) {}
+
+    CSVIterator(CSVIterator const & src) : file_(src.file_), item_pos_(src.item_pos_) {
+        this->item_ = (src.item_ == nullptr) ? nullptr : new ValueT(*(src.item_));
+    }
 
     ~CSVIterator() {
-        delete this->data_;
+        delete this->item_;
     }
+
     CSVIterator & operator++() {
-        this->file_.seekg(this->pos_);
+        skip(this->file_, this->item_pos_);
         std::string line;
         std::getline(this->file_, line);
-        this->pos_ += line.length() + 1;
+        this->file_.peek();
+        this->item_pos_ = (this->file_.eof()) ? this->EOF_POS : this->item_pos_ + line.length() + 1;
 
         std::cout << line << std::endl;
         return *this;
     }
+
     bool operator!=(CSVIterator const & other) const {
-        return this->pos_ != other.pos_;
+        return this->item_pos_ != other.item_pos_;
     }
+
     bool operator==(CSVIterator const & other) const {
-        return this->pos_ == other.pos_;
+        return this->item_pos_ == other.item_pos_;
     }
+
     typename CSVIterator::reference operator*() const {
-        return *(this->data_);
+        return *(this->item_);
     }
+
     typename CSVIterator::pointer operator->() const {
-        return this->data_;
+        return this->item_;
     }
 
     CSVIterator() = delete;
     CSVIterator & operator=(CSVIterator const &) = delete;
 private:
     std::ifstream & file_;
-    ValueT * data_;
-    size_t pos_;
+    ssize_t item_pos_;
+    ValueT * item_;
 };
 
 template <typename... Types>
@@ -51,28 +73,33 @@ public:
     using iterator = CSVIterator<value_type>;
     using const_iterator = CSVIterator<value_type const>;
 
-    CSVParser(std::ifstream & file, size_t line_offset) : file_(file), start_pos_(0), end_pos_(0) {
-        file.seekg(0, std::ios_base::beg);
-        for (size_t i = 0; i != line_offset; ++i) {
-            std::string tmp;
-            std::getline(file, tmp);
-            this->start_pos_ += tmp.length() + 1;
-        }
-        file.seekg(0, std::ios_base::end);
-        this->end_pos_ = file.tellg();
+    CSVParser(std::ifstream & file, size_t line_offset) : file_(file), beg_pos_(0) {
+        skip_lines(this->file_, line_offset);
+        this->beg_pos_ = this->file_.tellg();
     };
 
-    iterator begin() { return iterator(this->file_, this->start_pos_); }
-    iterator end() { return iterator(this->file_, this->end_pos_); }
-    const_iterator begin() const { return const_iterator(this->file_, this->start_pos_); }
-    const_iterator end() const { return const_iterator(this->file_, this->end_pos_); }
+    iterator begin() {
+        return iterator(this->file_, this->beg_pos_);
+    }
+
+    iterator end() {
+        return iterator(this->file_, iterator::EOF_POS);
+    }
+
+    const_iterator begin() const {
+        return const_iterator(this->file_, this->beg_pos_);
+    }
+
+    const_iterator end() const {
+        return const_iterator(this->file_, const_iterator::EOF_POS);
+    }
 
     ~CSVParser() = default;
+
     CSVParser() = delete;
     CSVParser(CSVParser const &) = delete;
     CSVParser & operator=(CSVParser const &) = delete;
 private:
     std::ifstream & file_;
-    size_t start_pos_;
-    size_t end_pos_;
+    size_t beg_pos_;
 };

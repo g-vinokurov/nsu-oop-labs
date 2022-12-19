9d3e8e36396d7c0033fd15a3339944f303b032c3
Georgiy Vinokurov <g.vinokurov@g.nsu.ru>
2022-12-19 16:38:39 +0700

    csv-parser was done

--- lab4/csv_parser.hpp
+++ lab4/csv_parser.hpp
@@ -1,27 +1,57 @@
 #pragma once
 
 #include <iterator>
+#include <sstream>
 #include <fstream>
 #include <limits>
 #include <string>
 #include <tuple>
+#include <vector>
 
-void skip_lines(std::ifstream & file, size_t n) {
+#include "tuple_printer.hpp"
+
+template <typename Type>
+std::tuple<Type> convert_to_tuple(std::vector<std::string> & data) {
+    std::istringstream str(data.front());
+    data.erase(data.begin());
+    Type tmp;
+    str >> tmp;
+    return std::make_tuple(tmp);
+}
+
+template <typename FirstType, typename SecondType, typename... Types>
+std::tuple<FirstType, SecondType, Types...> convert_to_tuple(std::vector<std::string> & data) {
+    std::istringstream str(data.front());
+    data.erase(data.begin());
+    FirstType tmp;
+    str >> tmp;
+    return std::tuple_cat(std::make_tuple(tmp), convert_to_tuple<SecondType, Types...>(data));
+}
+
+std::streamoff skip_lines(std::ifstream & file, size_t n) { // need testing on another platform
     file.clear(); file.seekg(0, std::ios_base::beg);
     for (size_t i = 0; i != n; ++i)
         file.ignore(std::numeric_limits<std::streamsize>::max(), file.widen('\n'));
+    return file.tellg();
 }
 
 void skip(std::ifstream & file, std::streamoff pos) {
     file.clear(); file.seekg(pos);
 }
 
-template <typename ValueT>
+template <typename ValueT, typename... Types>
 class CSVIterator final : public std::iterator<std::input_iterator_tag, ValueT, ssize_t> {
 public:
     static ssize_t const EOF_POS = -1;
 
-    CSVIterator(std::ifstream & file, ssize_t pos) : file_(file), item_pos_(pos), item_(nullptr) {}
+    CSVIterator(std::ifstream & file, ssize_t pos) : file_(file), item_pos_(pos), item_(nullptr) {
+        if (this->item_pos_ != this->EOF_POS) {
+            skip(this->file_, this->item_pos_);
+            std::string line;
+            std::getline(this->file_, line);
+            this->item_ = this->parse_line(line);
+        }
+    }
 
     CSVIterator(CSVIterator const & src) : file_(src.file_), item_pos_(src.item_pos_) {
         this->item_ = (src.item_ == nullptr) ? nullptr : new ValueT(*(src.item_));
@@ -31,14 +61,23 @@ public:
         delete this->item_;
     }
 
-    CSVIterator & operator++() {
+    CSVIterator & operator++() { // need testing on another platform
+        if (this->item_pos_ == this->EOF_POS)
+            return *this;
         skip(this->file_, this->item_pos_);
-        std::string line;
-        std::getline(this->file_, line);
-        this->file_.peek();
-        this->item_pos_ = (this->file_.eof()) ? this->EOF_POS : this->item_pos_ + line.length() + 1;
+        std::string curr_line, next_line;
+        std::getline(this->file_, curr_line);
+        this->item_pos_ += curr_line.length() + 1;
 
-        std::cout << line << std::endl;
+        std::getline(this->file_, next_line);
+        this->item_pos_ = (this->file_.eof()) ? this->EOF_POS : this->item_pos_;
+
+        delete this->item_;
+
+        if (this->item_pos_ != this->EOF_POS)
+            this->item_ = this->parse_line(next_line);
+        else
+            this->item_ = nullptr;
         return *this;
     }
 
@@ -61,6 +100,18 @@ public:
     CSVIterator() = delete;
     CSVIterator & operator=(CSVIterator const &) = delete;
 private:
+    std::tuple<Types...> * parse_line(std::string & line) {
+        std::vector<std::string> cells;
+        std::istringstream str(line);
+        while (!str.fail()) {
+            std::string tmp;
+            std::getline(str, tmp, str.widen(','));
+            cells.push_back(tmp);
+        }
+        cells.pop_back();
+        return new std::tuple<Types...>(convert_to_tuple<Types...>(cells));
+    }
+
     std::ifstream & file_;
     ssize_t item_pos_;
     ValueT * item_;
@@ -70,12 +121,11 @@ template <typename... Types>
 class CSVParser final {
 public:
     using value_type = std::tuple<Types...>;
-    using iterator = CSVIterator<value_type>;
-    using const_iterator = CSVIterator<value_type const>;
+    using iterator = CSVIterator<value_type, Types...>;
+    using const_iterator = CSVIterator<value_type const, Types...>;
 
     CSVParser(std::ifstream & file, size_t line_offset) : file_(file), beg_pos_(0) {
-        skip_lines(this->file_, line_offset);
-        this->beg_pos_ = this->file_.tellg();
+        this->beg_pos_ = skip_lines(this->file_, line_offset);
     };
 
     iterator begin() {
--- lab4/main.cpp
+++ lab4/main.cpp
@@ -3,12 +3,12 @@
 #include <string>
 
 #include "csv_parser.hpp"
+#include "tuple_printer.hpp"
 
 int main(int argc, char ** argv) {
     std::ifstream file("file.csv");
-    CSVParser<int, std::string> parser(file, 0);
-    CSVParser<int, std::string>::iterator p = parser.begin();
-    CSVParser<int, std::string>::iterator q = parser.end();
-    for (; p != q; ++p);
+    CSVParser<int, int, int, int, int, int, int, double, int> parser(file, 1);
+    for (std::tuple<int, int, int, int, int, int, int, double, int> item : parser)
+        std::cout << item << std::endl;
     return 0;
 }
